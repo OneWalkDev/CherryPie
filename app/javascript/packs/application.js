550a81a049bbfa4e92c8b963343cf5092e5c42ce
yurisi0212 <Dev.yurisi@gmail.com>
2021-11-08 01:11:40 +0900

    application.scssを変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 
-import "bootstrap";
-import "../stylesheets/application";
+import 'bootstrap';
+import '../stylesheets/application';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
